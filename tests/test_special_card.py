89c95a6f3d272d1a551a78e2d871a66b36ccaa7e
samraber <sam9985246327@gmail.com>
2025-12-16 17:27:06 +1030

    Update testing

--- tests/test_special_card.py
+++ tests/test_special_card.py
@@ -5,12 +5,12 @@ import json
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-from generate_card_json import generate_json
-from check_duplicate_cards import check_duplicate_cards
-from generate_special_card_json import generate_special_card_data
+from scripts import generate_json, generate_special_card_data
+from src.services import check_duplicate_cards
 
-image_folder = r"./A1-test-jp"
-duplicates_list = r"./A1_duplicates.json"
+image_folder = r"./tests/A1-test-jp"
+duplicates_list = r"./tests/A1_duplicates.json"
+non_pokemon_booster_pack = {}
 
 # Verify image exists
 if not os.path.exists(image_folder):
@@ -20,13 +20,18 @@ if not os.path.exists(image_folder):
 class TestCardGenerator(unittest.TestCase):
     def test_card_generator(self):
         # Expected output
-        with open(r"./A1_expected_result.json", "r") as f:
+        with open(r"./tests/A1_expected_result.json", "r") as f:
             expected_data = json.load(f)
 
         # Run the generation
-        result = generate_json(
+        global non_pokemon_booster_pack
+        result, non_pokemon_booster_pack = generate_json(
             image_folder,
-            ["./A1_Charizard.xlsx", "./A1_Mewtwo.xlsx", "./A1_Pikachu.xlsx"],
+            [
+                "./tests/A1_Charizard.xlsx",
+                "./tests/A1_Mewtwo.xlsx",
+                "./tests/A1_Pikachu.xlsx",
+            ],
         )
 
         # Assertions
@@ -40,7 +45,7 @@ class TestDuplicateCard(unittest.TestCase):
             expected_data = json.load(f)
 
         # Run the generation
-        result = check_duplicate_cards("./A1_expected_result.json")
+        result = check_duplicate_cards("./tests/A1_expected_result.json")
 
         # Assertions
         self.assertEqual(result, expected_data)
@@ -50,11 +55,22 @@ class TestSpecialCard(unittest.TestCase):
     def test_special_card(self):
 
         # Expected output
-        with open(r"./A1_expected_special_result.json", "r") as f:
+        with open(r"./tests/A1_expected_special_result.json", "r") as f:
             expected_data = json.load(f)
 
         # Run the generation
         result = generate_special_card_data(image_folder, duplicates_list)
+
+        # Combine the non pokemon booster pack
+        for key, value in non_pokemon_booster_pack.items():
+            if (
+                key in result
+                and isinstance(result[key], dict)
+                and isinstance(value, dict)
+            ):
+                result[key].update(value)
+            else:
+                result[key] = value
 
         # Assertions
         self.assertEqual(result, expected_data)
